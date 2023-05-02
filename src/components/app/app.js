6605bf4c4b2f4829a4fe90bccc313be6d70ab284
Daniil Mitrenin <d.mitt@yandex.ru>
2023-05-02 20:13:49 +0700

    BurgerConstructor layout

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,5 @@
 import styles from "./app.module.css";
+import { clsx } from "clsx";
 import AppHeader from "../app-header/app-header";
 import BurgerConstructor from "../burger-constructor/burger-constructor";
 import BurgerIngredients from "../burger-ingredients/burger-ingredients";
@@ -9,7 +10,7 @@ function App() {
       <AppHeader />
       <main className={styles.content}>
         <BurgerIngredients />
-        {/*  <BurgerConstructor />*/}
+        <BurgerConstructor />
       </main>
     </div>
   );
--- src/components/burger-constructor/burger-constructor.js
+++ src/components/burger-constructor/burger-constructor.js
@@ -1,34 +1,85 @@
-import React from "react";
-import { ConstructorElement } from "@ya.praktikum/react-developer-burger-ui-components";
+import React, { useState } from "react";
+import {
+  ConstructorElement,
+  DragIcon,
+} from "@ya.praktikum/react-developer-burger-ui-components";
 import data from "../../utils/data";
+import styles from "./burger-constructor.module.css";
+import { clsx } from "clsx";
 
-//Представим withToggle в виде обычного функционального компонента.
 const BurgerConstructor = () => {
-  const img = data[0].image;
-
+  const [elements, setElements] = useState([
+    data[0],
+    data[1],
+    data[1],
+    data[1],
+    data[1],
+    data[1],
+    data[1],
+    data[1],
+    data[1],
+    data[1],
+    data[1],
+    data[1],
+    data[1],
+  ]);
+  const elementBun = elements.find((elem) => elem.type === "bun");
+  const elementsFilling = elements.filter((elem) => elem.type !== "bun");
+  // const [elementBun, setElementBun] = useState(null);
   return (
-    <section>
-      <div style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
-        <ConstructorElement
-          type="top"
-          isLocked={true}
-          text="Краторная булка N-200i (верх)"
-          price={200}
-          thumbnail={img}
-        />
-        <ConstructorElement
-          text="Краторная булка N-200i (верх)"
-          price={50}
-          thumbnail={img}
-        />
-        <ConstructorElement
-          type="bottom"
-          isLocked={true}
-          text="Краторная булка N-200i (низ)"
-          price={200}
-          thumbnail={img}
-        />
-      </div>
+    <section className={clsx(styles.burgerConstructor, "pt-15 pl-4 pr-4")}>
+      <ul className={styles.burgerElements}>
+        {elementBun && (
+          <>
+            <li className={clsx(styles.bunTop, "ml-8")}>
+              <ConstructorElement
+                type="top"
+                isLocked={true}
+                text={elementBun.name}
+                price={elementBun.price}
+                thumbnail={elementBun.image}
+                key={elementBun._id}
+              />
+            </li>
+            <li className={clsx(styles.bunBottom, "ml-8")}>
+              <ConstructorElement
+                type="bottom"
+                isLocked={true}
+                text={elementBun.name}
+                price={elementBun.price}
+                thumbnail={elementBun.image}
+                key={elementBun._id}
+              />
+            </li>
+          </>
+        )}
+        <li>
+          <ul className={clsx(styles.burgerFilling, "custom-scroll")}>
+            {elementsFilling.map((element) => {
+              const { type, name, price, image, _id } = element;
+              return (
+                <li className={clsx(styles.burgerElement)}>
+                  <DragIcon type={"primary"} />
+                  <ConstructorElement
+                    text={name}
+                    price={price}
+                    thumbnail={image}
+                    key={_id}
+                  />
+                </li>
+              );
+            })}
+          </ul>
+        </li>
+        {/*<ConstructorElement*/}
+        {/*  type="bottom"*/}
+        {/*  isLocked={true}*/}
+        {/*  text={elementBun.name}*/}
+        {/*  price={elementBun.price}*/}
+        {/*  thumbnail={elementBun.image}*/}
+        {/*  key={elementBun._id}*/}
+        {/*/>*/}
+      </ul>
     </section>
   );
 };
