7b1fb29e9d9c521aa118cb4cc5011e1428cf9b5c
Daniil Mitrenin <d.mitt@yandex.ru>
2023-05-03 19:56:17 +0700

    burgerConstructor layout

--- src/components/burger-constructor/burger-constructor.js
+++ src/components/burger-constructor/burger-constructor.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import {
+  Button,
   ConstructorElement,
+  CurrencyIcon,
   DragIcon,
 } from "@ya.praktikum/react-developer-burger-ui-components";
 import data from "../../utils/data";
@@ -10,20 +12,26 @@ import { clsx } from "clsx";
 const BurgerConstructor = () => {
   const [elements, setElements] = useState([
     data[0],
-    data[1],
     // data[1],
     // data[1],
+    // data[1],
+    // data[1],
+    // data[2],
+    // data[2],
     data[1],
-    data[2],
+    data[1],
+    data[1],
     data[1],
     data[3],
+    data[3],
   ]);
+  const [sum, setSum] = useState(0);
   const elementBun = elements.find((elem) => elem.type === "bun");
   const elementsFilling = elements.filter((elem) => elem.type !== "bun");
   // const [elementBun, setElementBun] = useState(null);
   return (
     <section className={clsx(styles.burgerConstructor, "pt-15 pl-4 pr-4")}>
-      <ul className={styles.burgerElements}>
+      <ul className={clsx(styles.burgerElements, "mb-10")}>
         {elementBun &&
           ["Top", "Bottom"].map((element) => (
             <li className={clsx(styles[`bun${element}`], "ml-8")}>
@@ -37,12 +45,12 @@ const BurgerConstructor = () => {
               />
             </li>
           ))}
-        <li>
-          <ul className={clsx(styles.burgerFilling, "custom-scroll")}>
+        <li className={styles.burgerFilling}>
+          <ul className={clsx(styles.burgerFillingList, "custom-scroll")}>
             {elementsFilling.map((element) => {
               const { name, price, image, _id } = element;
               return (
-                <li className={clsx(styles.burgerElement)}>
+                <li className={clsx(styles.burgerFillingElement)}>
                   <DragIcon type={"primary"} />
                   <ConstructorElement
                     text={name}
@@ -56,6 +64,15 @@ const BurgerConstructor = () => {
           </ul>
         </li>
       </ul>
+      <div className={clsx(styles.info)}>
+        <span className={clsx("text text_type_digits-medium", styles.sum)}>
+          <p className={"text"}>{sum}</p>
+          <CurrencyIcon />
+        </span>
+        <Button htmlType="button" type="primary" size="large">
+          Оформить заказ
+        </Button>
+      </div>
     </section>
   );
 };
